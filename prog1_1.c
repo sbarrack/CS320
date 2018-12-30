2795ffa6ac93665bcc2fc757943da0c3712670f4
Stephen <12619603+sbarrack@users.noreply.github.com>
2018-12-29 19:54:32 -0800

    prog1

---
+++ prog1_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main( ) {
+    
+    char string[100];
+    
+    printf("Assignment #1-1, Stephen Barrack\n");
+    printf("What is your name?\n");
+    gets(string);
+    printf("Hello %s!\n", string);
+    
+    return 0;
+}
